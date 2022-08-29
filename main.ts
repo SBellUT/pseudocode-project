2540d86daf098083cba961b6421f32c3d2ada96c
SBellUT <111801926+SBellUT@users.noreply.github.com>
2022-08-29 12:45:34 -0600

    Finished Project

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("great!")
+})
 basic.forever(function () {
 	
 })
